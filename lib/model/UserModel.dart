8cab08050c4544caa2cbde1e4b9b6e0a0f2f856d
ronikarki <ronishakarki2000@gmail.com>
2022-05-14 20:03:05 +0545

    Google Signin

---
+++ lib/model/UserModel.dart
@@ -0,0 +1,8 @@
+class User {
+  final String? firstname;
+  final String? lastname;
+  final String? password;
+  final String? email;
+
+  User({this.firstname, this.lastname, this.password, this.email});
+}
